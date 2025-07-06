078ff3cd2b8afa195a02f90971b9b0e6741bf41c
manshyna <0951719224s@gmail.com>
2025-07-06 17:02:43 +0200

    HW2-1

---
+++ HomeWork/Homework2/HW2-1/hw2-1.js
@@ -0,0 +1,14 @@
+let orders= ['qsw', 123, 345, 'noname', 'email', 'phone', 'password', 'new', 321, 80 ];
+console.log(orders[0]);
+console.log(orders[1]);
+console.log(orders[2]);
+console.log(orders[3]);
+console.log(orders[4]);
+console.log(orders[5]);
+console.log(orders[6]);
+console.log(orders[7]);
+console.log(orders[8]);
+console.log(orders[9]);
+
+
+
