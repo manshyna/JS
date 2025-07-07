3393b5ab025948fc78c4ba9fe961ea69eb23cac0
manshyna <0951719224s@gmail.com>
2025-07-07 13:44:19 +0200

    HW2-2

---
+++ HomeWork/Homework2/HW2-2/hw2-2.js
@@ -0,0 +1,6 @@
+let book1=
+    {title: 'oa' ,pageCount:180, genre:'mystery'};
+let book2=
+    {title: 'blood' ,pageCount:145, genre:'thriller'};
+let book3=
+    {title: 'elf' ,pageCount:320, genre:'fantasy'};
