da7353c9046c3d2eed4395d8947f0e7c0e408bbb
manshyna <0951719224s@gmail.com>
2025-07-07 14:15:31 +0200

    HW2-4

---
+++ HomeWork/Homework2/HW2-4/hw2-4.js
@@ -0,0 +1,23 @@
+let users = [
+    {name: 'Alex', username: 'Rok', password: 'dfd5'},
+    {name: 'Anna', username: 'Kalo', password: 'dsh6'},
+    {name: 'Olga', username: 'Nerit', password: 'alku8'},
+    {name: 'Den', username: 'Ivanov', password: 'ssk7'},
+    {name: 'Jan', username: 'Jorev', password: '234fc'},
+    {name: 'San', username: 'Moler', password: 'dfgh2'},
+    {name: 'Borys', username: 'Hofat', password: 'iioak4'},
+    {name: 'Ira', username: 'Meriot', password: 'hfd4t'},
+    {name: 'Sam', username: 'Link', password: ';p[vt6'},
+    {name: 'Ivan', username: 'Stew', password: 'khd5s'},
+]
+console.log(users[0])
+console.log(users[1])
+console.log(users[2])
+console.log(users[3])
+console.log(users[4])
+console.log(users[5])
+console.log(users[6])
+console.log(users[7])
+console.log(users[8])
+console.log(users[9])
+
