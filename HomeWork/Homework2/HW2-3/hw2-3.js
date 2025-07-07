83462b1679c168720b34c561319adee1dc94049c
manshyna <0951719224s@gmail.com>
2025-07-07 14:01:00 +0200

    HW2-3

---
+++ HomeWork/Homework2/HW2-3/hw2-3.js
@@ -0,0 +1,28 @@
+let book1= {
+    title: 'oa',
+    pageCount:180,
+    genre:'mystery',
+    authors:[
+        {name: 'Alex Ron', age: 35},
+        {name: 'Jan Ron', age: 45}
+    ]};
+
+let book2= {
+    title: 'blood',
+    pageCount:145,
+    genre:'thriller',
+    authors:[
+        {name: 'Alex Full', age: 56},
+        {name: 'Jan ion', age: 45},
+        {name: 'San Francisco', age: 35}
+    ]};
+let book3= {
+    title: 'elf',
+    pageCount:320,
+    genre:'fantasy',
+    authors:[
+        {name: 'Anna Full', age: 46},
+        {name: 'San ion', age: 45},
+        {name: 'Low Wer', age: 68}
+    ]};
+
