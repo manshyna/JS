b30da921970c36e2ba2d9e1f1427375310d780b2
manshyna <0951719224s@gmail.com>
2025-07-07 16:46:12 +0200

    HW2-5

---
+++ HomeWork/HW2-5/hw2-5.js
@@ -0,0 +1,29 @@
+let daytemps=[
+    {day:'monday', morningTemp: 10, afternoonTemp:20, nightTemp:5},
+    {day:'tuesday', morningTemp: 12, afternoonTemp:23, nightTemp:10},
+    {day:'wednesday', morningTemp: 8, afternoonTemp:18, nightTemp:5},
+    {day:'thursday', morningTemp: 12, afternoonTemp:22, nightTemp:5},
+    {day:'friday', morningTemp: 10, afternoonTemp:23, nightTemp:15},
+    {day:'saturday', morningTemp: 11, afternoonTemp:20, nightTemp:6},
+    {day:'sunday', morningTemp: 9, afternoonTemp:21, nightTemp:10},
+]
+
+
+
+
+let temps=[
+    {day:'monday', valueTemp:
+            {morning: 10, afternoon:20, night:5}},
+    {day:'tuesday', valueTemp:
+            {morning: 12, afternoon:23, night:10}},
+    {day:'wednesday', valueTemp:
+            {morning: 8, afternoon:24, night:10}},
+    {day:'thursday', valueTemp:
+            {morning: 12, afternoon:22, night:5}},
+    {day:'friday', valueTemp:
+            {morning: 17, afternoon:27, night:18}},
+    {day:'saturday', valueTemp:
+            {morning: 19, afternoon:26, night:15}},
+    {day:'sunday', valueTemp:
+            {morning: 15, afternoon:24, night:12}}
+]
