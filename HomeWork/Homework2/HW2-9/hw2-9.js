46896949221d465237baf739366cd7ce7a1dfa47
manshyna <0951719224s@gmail.com>
2025-07-08 21:20:04 +0200

    HW2-9

---
+++ HomeWork/Homework2/HW2-9/hw2-9.js
@@ -0,0 +1,45 @@
+
+switch ('friday'){
+    case 'monday':
+        console.log('Monday day schedule');
+        console.log(1);
+        console.log(2);
+        console.log(3);
+        break;
+    case 'tuesday':
+        console.log('Tuesday day schedule');
+        console.log(1);
+        console.log(2);
+        console.log(3);
+        break;
+    case 'wednesday':
+        console.log('Wednesday day schedule');
+        console.log(1);
+        console.log(2);
+        console.log(3);
+        break;
+    case 'thursday':
+        console.log('Thursday day schedule');
+        console.log(1);
+        console.log(2);
+        console.log(3);
+        break;
+    case 'friday':
+        console.log('Friday day schedule');
+        console.log(1);
+        console.log(2);
+        console.log(3);
+        break;
+    case 'saturday':
+        console.log('Saturday day schedule');
+        console.log(1);
+        console.log(2);
+        console.log(3);
+        break;
+    case 'sunday':
+        console.log('Sunday day schedule');
+        console.log(1);
+        console.log(2);
+        console.log(3);
+        break;
+}
