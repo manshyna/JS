14dd2a1af4886b15481b2c4c387015a2d816d6b1
manshyna <0951719224s@gmail.com>
2025-07-08 16:55:11 +0200

    HW2-8

---
+++ HomeWork/Homework2/HW2-8/hw2-8.js
@@ -0,0 +1,14 @@
+
+
+
+    let day = 11
+    if (day >=1 && day <=10) {
+        console.log('1');
+    }
+    else if (day >10 && day <=20) {
+        console.log('2');
+    }
+    else if (day >20 && day <=30) {
+        console.log('3');
+    }
+
