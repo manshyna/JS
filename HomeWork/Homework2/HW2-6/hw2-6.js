1cd706215401c1647409fb4c0ec0fb7aad465997
manshyna <0951719224s@gmail.com>
2025-07-08 16:23:17 +0200

    HW2-6

---
+++ HomeWork/Homework2/HW2-6/hw2-6.js
@@ -0,0 +1,17 @@
+let x='dyegss'
+if (x !==0) {
+    console.log('right')
+}
+else {
+    console.log('wrong')
+}
+
+
+
+let a=1
+if (a !==0) {
+    console.log('right');
+}
+else {
+    console.log('wrong');
+}
