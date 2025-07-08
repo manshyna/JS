4d4d4a140c32a2849f5e8b291e90137508216bce
manshyna <0951719224s@gmail.com>
2025-07-08 16:42:39 +0200

    HW2-7

---
+++ HomeWork/Homework2/HW2-7/hw2-7.js
@@ -0,0 +1,13 @@
+let time= 16
+if (time > 0 && time <= 15) {
+    console.log('quarter 1');
+}
+else if (time >= 15 && time <=30) {
+    console.log('quarter 2');
+}
+else if (time >=30 && time <=45) {
+    console.log('quarter 3');
+}
+else if (time >=45 && time <=60) {
+    console.log('quarter 4');
+}
