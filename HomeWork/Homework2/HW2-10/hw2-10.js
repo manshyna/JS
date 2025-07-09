a424ce0c5da75dbb8e0f3e675329ba31d82b9c4c
manshyna <0951719224s@gmail.com>
2025-07-09 13:21:40 +0200

    HW2-10

---
+++ HomeWork/Homework2/HW2-10/hw2-10.js
@@ -0,0 +1,12 @@
+let a= +prompt('write number1')
+let b= +prompt('write number2')
+
+if (a>b){
+    console.log(a);
+}
+else if (a<b){
+    console.log(b);
+}
+else if (a===b){
+    console.log('ok');
+}
