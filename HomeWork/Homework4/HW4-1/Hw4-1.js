8bc3afd485dfa7a3075b71fbef8d484a28129031
manshyna <0951719224s@gmail.com>
2025-07-24 16:16:24 +0200

    HW4-1-2

---
+++ HomeWork/Homework4/HW4-1/Hw4-1.js
@@ -0,0 +1,9 @@
+
+function sum(a,b) {
+    let number = a * b;
+    return number;
+}
+
+let s = sum(1, 2);
+
+console.log(s);
---
+++ HomeWork/Homework4/HW4-2/Hw4-2.js
@@ -0,0 +1,6 @@
+function square (r) {
+    let result = 3.14*(r*r)
+    return result;
+}
+square(10)
+console.log(square)
